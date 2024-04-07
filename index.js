0a3d580b03f3a10aef83493c71f95d9c783b5660
dhanushramudri <ramudridhanush@gmail.com>
2024-04-07 22:20:46 +0530

    completed chat app

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require("express");
-
 const app = express();
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
+
+const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+  console.log("New connection");
+
+  socket.emit("message", "Welcome to the chat");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("join", ({ room, name }) => {
+    console.log("join", room, name);
+    socket.join(room);
+    io.to(room).emit("notification", `${name} has joined the ${room}`);
+  });
+
+  socket.on("messageRoom", ({ room, message }) => {
+    console.log("message", room, message);
+    io.to(room).emit("message", message);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
